7647ccb6aa3d66102cc33523d875efe46cda409e
Yue Jianli <jianli.yue@shopee.com>
2023-02-14 10:17:47 +0800

    feat: test mongo

--- pkg/mongo/mongo.go
+++ pkg/mongo/mongo.go
@@ -37,10 +37,17 @@ func NewClient(c *ClientConfig) (*Client, error) {
 
 func (c *Client) Find(ctx context.Context, collection string) (interface{}, error) {
 	filter := bson.D{{}}
-	if err := c.cli.Database(c.db).Collection(collection).FindOne(ctx, filter).Err(); err != nil {
-		fmt.Printf("find err: %s, db:%s\n", err.Error(), c.db)
+	r := c.cli.Database(c.db).Collection(collection).FindOne(ctx, filter)
+	if r.Err() != nil {
+		fmt.Printf("find err: %s, db:%s\n", r.Err().Error(), c.db)
+		return nil, r.Err()
+	}
+	raw, err := r.DecodeBytes()
+	if err != nil {
+		fmt.Printf("r.DecodeBytes err:%s\n", err.Error())
 		return nil, err
 	}
+	fmt.Printf("raw string:%s\n", raw.String())
 
 	// fmt.Printf("search %s, id:%d\n", c.db, cursor.ID())
 	// defer func() {
