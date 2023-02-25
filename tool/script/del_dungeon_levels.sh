35ae3fa60b05764969e6eac7cfedafab410aa662
will <958953720@qq.com>
2023-02-26 00:42:59 +0800

    feat: add shell

--- tool/script/del_dungeon_levels.sh
+++ tool/script/del_dungeon_levels.sh
@@ -3,8 +3,7 @@ go build -o main ../../app/campaigns
 ./main -method selected_level_id -level_file ../../doc/campaign/selected_level.txt
 awk -F"\t" '{print $2}' ../../doc/campaign/level_with_id.txt | while read row
 do
-  echo $row
-  #cmd='db.campaigns.update({"_id":ObjectId("549f07f7e21e041139ef28c7")}, {"$unset":{"levels.'$row'":""}})'
-  #echo $cmd
+  cmd='db.campaigns.update({"_id":ObjectId("549f07f7e21e041139ef28c7")}, {"$unset":{"levels.'$row'":""}})'
+  mongo --eval '$cmd' coco
 done
 rm main
