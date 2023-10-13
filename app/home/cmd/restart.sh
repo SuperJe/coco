82c10f5f8bd9b92009c67773f21e2c327865bbc2
Will <958953720@qq.com>
2023-10-13 20:38:35 +0800

    feat: 重启脚本完善

--- app/home/cmd/restart.sh
+++ app/home/cmd/restart.sh
@@ -4,7 +4,6 @@ rm ct_home
 go build -o ct_home main.go
 echo '编译成功'
 echo '原始进程号:'
-ps aux | grep "ct_home" | grep -v color | awk '{print $2}' | head -1
 ps aux | grep "ct_home" | grep -v color | awk '{print $2}'| head -1 | xargs kill
 echo '杀死原始进程成功, 准备拉起新进程...'
 nohup ./ct_home > ../../../log/ct_home.log 2>&1 & disown
